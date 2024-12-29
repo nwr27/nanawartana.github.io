66b447f56be3f7fd92b8d4903ecd066cf6bd18c7
nwr27 <nanawartana27@gmail.com>
2024-12-29 20:09:17 +0700

    -

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,3 +1,4 @@
+// Existing JavaScript
 document.querySelectorAll(".nav-link").forEach((link) => {
   link.addEventListener("click", () => {
     const navbarCollapse = document.querySelector(".navbar-collapse");
@@ -14,5 +15,31 @@ document.addEventListener("click", (event) => {
   if (navbar.classList.contains("show") && !navbar.contains(event.target) && !toggler.contains(event.target)) {
     const bsCollapse = new bootstrap.Collapse(navbar, { toggle: true });
     bsCollapse.hide();
+  }
+});
+
+// Tambahkan JavaScript baru di sini
+
+// Mengganti gambar saat di-hover dan mengembalikannya saat tidak di-hover
+const profileImg = document.getElementById("profile-img");
+const originalSrc = "static/img/nana.jpg";
+const hoverSrc = "static/img/nana2.jpg";
+
+profileImg.addEventListener("mouseover", () => {
+  profileImg.src = hoverSrc;
+});
+
+profileImg.addEventListener("mouseout", () => {
+  profileImg.src = originalSrc;
+});
+
+// Mengganti gambar saat diklik (toggle)
+profileImg.addEventListener("click", () => {
+  if (profileImg.classList.contains("active")) {
+    profileImg.src = originalSrc;
+    profileImg.classList.remove("active");
+  } else {
+    profileImg.src = hoverSrc;
+    profileImg.classList.add("active");
   }
 });
