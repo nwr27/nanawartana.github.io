6d42dab1ebf73d0d8870a9ba9bba14aa10074c28
nwr27 <nanawartana27@gmail.com>
2025-01-11 10:04:24 +0700

    hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,17 @@
 module.exports = {
   content: ["index.html"],
   theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
     extend: {
       colors: {
         primary: "#f97316",
+        dark: "#0f172a",
+      },
+      screens: {
+        "2xl": "1280px",
       },
     },
   },
