896bc3dc718bf56c08a9291d6c54ae778ab475f6
nwr27 <nanawartana27@gmail.com>
2025-01-12 23:57:21 +0700

    database google sheets

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -15,5 +15,25 @@ const navMenu = document.querySelector("#nav-menu");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("hamburger-active");
-  navMenu.classList.toggle("hidden")
+  navMenu.classList.toggle("hidden");
+});
+
+// Form
+
+document.getElementById("contact").addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const formData = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+
+  const response = await fetch("https://script.google.com/macros/s/AKfycbwyq3DsJtKch9bdD11K_BM0fLuSKYlSdm1cA0krCFbFf0RyToTDrecltr6g_m69DfK1fw/exec", {
+    method: "POST",
+    body: JSON.stringify(formData),
+  });
+
+  const result = await response.json();
+  alert(result.message);
 });
